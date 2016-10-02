0f1fa914c56ff95b61bbd6d5122ab9775b57f056
Olzhas Adiyatov <olzhas.adi@gmail.com>
2016-10-02 23:05:37 +0600

    :lipstick:

--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 
 #include "pid.hpp"
 
-int main(int argc, char *argv[])
-{
-    DiscretePID pid(0.1, 0, 0);
+int main(int argc, char *argv[]) {
+  DiscretePID pid(0.1, 0, 0);
 
-  //pid.assignReadFunc(std::bind);
-  //pid.assignWriteFunc(std::bind);
+  // pid.assignReadFunc(std::bind);
+  // pid.assignWriteFunc(std::bind);
 
   pid.setReference(1);
 
-    return 0;
+  return 0;
 }
